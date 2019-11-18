20b8fa86a6cd0a6584c63d893f2e98df434ee6aa
John Soo <jsoo1@asu.edu>
2019-11-18 08:51:15 -0800

    More dir-locals tweaks.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,17 @@
-((haskell-mode . ((haskell-process-type . 'cabal-repl)
-                  (haskell-mode-stylish-haskell-path . "ormolu")
-                  (haskell-stylish-on-save . 't)))
- (nil . ((eval . (setq
-                  projectile-compilation-command "cabal new-build"
-                  haskell-process-wrapper-function (lambda (argv) (append '("env" "-u" "GHC_PACKAGE_PATH") argv)))))))
+((nil
+  . ((eval
+      . (setq
+         projectile-project-compilation-cmd
+         (concat
+          "docker run -it"
+          " --volume " (projectile-project-root) ":/src "
+          "refl.club-build "
+          "cabal new-build")
+         haskell-process-wrapper-function
+         (lambda (argv)
+           (append
+            `("env" "-u" "GHC_PACKAGE_PATH") argv))))))
+ (haskell-mode
+  . ((haskell-process-type . cabal-new-repl)
+     (haskell-mode-stylish-haskell-path . "ormolu")
+     (haskell-stylish-on-save . t))))
