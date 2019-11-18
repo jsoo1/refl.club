ada5e353d6cd33b8f46bdaf6d881884d3828a59b
John Soo <jsoo1@asu.edu>
2019-11-17 17:53:12 -0800

    Add ormolu, cabal, haskell process wrapper, compile command.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
-((haskell-mode . ((haskell-process-type . 'ghci)
+((haskell-mode . ((haskell-process-type . 'cabal-repl)
+                  (haskell-process-wrapper-function . (lambda (argv) (append '("env" "-u" "GHC_PACKAGE_PATH") argv)))
                   (haskell-mode-stylish-haskell-path . "ormolu")
-                  (haskell-stylish-on-save . 't))))
+                  (haskell-stylish-on-save . 't)))
+ (nil . ((eval . (setq projectile-compilation-command "cabal new-build")))))
