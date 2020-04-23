39033c941606cb4486920c6fe01f3bbe28eb2f3f
John Soo <jsoo1@asu.edu>
2020-04-22 22:46:25 -0700

    Make a working version for GCP.

--- gcp/Main.hs
+++ gcp/Main.hs
@@ -1,8 +1,27 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
+import Lucid
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Lucid
 import System.Environment
+
+type Club = Get '[HTML] ()
+
+instance ToHtml () where
+  toHtml _ =
+    html_
+      $ body_
+      $ h1_ "hello"
+
+club :: Server Club
+club = pure ()
 
 main :: IO ()
 main = do
-  port <- getEnv "PORT"
-  print port
+  port <- read @Int <$> getEnv "PORT"
+  run port $ serve @Club Proxy club
