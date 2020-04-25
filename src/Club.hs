daf08d853bfbe9d7388b4c00ecbe74cec8ed97f6
John Soo <jsoo1@asu.edu>
2020-04-25 11:57:09 -0700

    Move pages and api to library.

---
+++ src/Club.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Club (Club, clubApi, club) where
+
+import Lucid
+import About (About(..))
+import Servant ((:<|>)(..), Proxy(..), Get, Raw, Server, serveDirectoryWebApp)
+import Servant.HTML.Lucid (HTML)
+
+type Club =
+  Get '[HTML] About :<|> Raw
+
+clubApi :: Proxy Club
+clubApi = Proxy
+
+club :: String -> Server Club
+club staticDir =
+  pure About :<|> serveDirectoryWebApp staticDir
