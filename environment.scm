17707a9aaebe9bb7f7e299c52b79b0abef2944c1
John Soo <jsoo1@asu.edu>
2020-04-25 11:26:22 -0700

    Tidy up environment. Add .projectile for the projectile users.

--- environment.scm
+++ environment.scm
@@ -1,15 +1,7 @@
 (use-modules
- (ghc-aws-lambda-haskell-runtime)
- (ghc-lucid)
- (gnu packages compression)
  (gnu packages haskell-apps)
- (gnu packages haskell-web)
  (ormolu))
 
-`(,ghc-aeson
-  ,ghc-lucid
-  ,ghc-aws-lambda-haskell-runtime
-  ,hlint
-  ,lzlib
-  ,zlib
-  ,ormolu)
+(list
+ ghcid
+ ormolu)
