4202094814583130bb74112814e08f8a061ab171
John Soo <jsoo1@asu.edu>
2020-05-04 14:58:31 -0700

    Add GPL3.

--- aws/Index.hs
+++ aws/Index.hs
@@ -1,18 +1,33 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ExistentialQuantification #-}
-
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+
+-- This file is part of refl.club - a personal website and blog
+-- Copyright (C) 2020 John Soo <jsoo1@asu.edu>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 module Index where
 
 import AWS.Lambda
-import Control.Monad.Trans (liftIO)
 import Control.Monad.Reader (ask)
-import Data.Aeson
+import Control.Monad.Trans (liftIO)
 import qualified Data.AWS.Error as AWS
 import Data.AWS.Runtime
 import qualified Data.AWS.Runtime.Response as Response
 import qualified Data.AWS.Startup as Startup
+import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import qualified Data.Text.Lazy as TL
 import GHC.Generics
@@ -22,8 +37,8 @@ type MonadIndex = MonadLambda () Startup.Error IO Response
 
 lambda :: Lambda IO
 lambda = Lambda
-  { lambdaSetup = setup 
-  , lambdaHandler = handler
+  { lambdaSetup = setup,
+    lambdaHandler = handler
   }
 
 setup :: Startup.Env -> IO (Either Startup.Error ())
--- aws/Main.hs
+++ aws/Main.hs
@@ -1,10 +1,26 @@
 {-# LANGUAGE LambdaCase #-}
 
+-- This file is part of refl.club - a personal website and blog
+-- Copyright (C) 2020 John Soo <jsoo1@asu.edu>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 module Main where
 
 import qualified AWS
-import Data.AWS.Startup as Startup
 import qualified Data.AWS.Error as AWS
+import Data.AWS.Startup as Startup
 import Data.Text (Text)
 import qualified Index
 import System.IO
@@ -17,10 +33,10 @@ main = do
 
 runLambdaFunctions :: Startup.Env -> IO ()
 runLambdaFunctions env =
-  either AWS.failOnInit (AWS.lambdaLoop env)
-  $ resolveLambda (Startup.handler env)
+  either AWS.failOnInit (AWS.lambdaLoop env) $
+    resolveLambda (Startup.handler env)
 
 resolveLambda :: Text -> Either Startup.Error (AWS.Lambda IO)
 resolveLambda = \case
-    "Index.handler" -> pure Index.lambda
-    handler -> Left $ Startup.HandlerNotFound handler
+  "Index.handler" -> pure Index.lambda
+  handler -> Left $ Startup.HandlerNotFound handler
