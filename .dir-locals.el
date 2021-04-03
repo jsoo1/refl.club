cc8496a2ea01231b1b10c1e6e1a35036e64026cc
John Soo <jsoo1@asu.edu>
2021-04-03 07:54:39 -0700

    Fix ormolu arguments

--- .dir-locals.el
+++ .dir-locals.el
@@ -12,5 +12,5 @@
  (haskell-mode
   . ((haskell-process-type . cabal-repl)
      (haskell-mode-stylish-haskell-path . "ormolu")
-     (haskell-mode-stylish-haskell-args . ("--ghc-opt TypeApplications"))
+     (haskell-mode-stylish-haskell-args . ("--ghc-opt" "TypeApplications"))
      (haskell-stylish-on-save . t))))
