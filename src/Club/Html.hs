ffeea9c7913e5d9b5ae791d99065eed6cbc280fd
John Soo <jsoo1@asu.edu>
2021-04-19 12:31:25 -0700

    Format Html.

--- src/Club/Html.hs
+++ src/Club/Html.hs
@@ -22,20 +22,21 @@ navBar curr =
 
 waitOnFonts :: Monad m => HtmlT m ()
 waitOnFonts =
-  script_ "document.fonts.ready.then(fonts => {\
-          \\n  let fontFamilies = Array.from(fonts.values()).map(f => f.family)\
-          \\n  if (fontFamilies.includes('Iosevka')) {\
-          \\n    document.querySelectorAll('pre').forEach(e => { \
-          \\n      e.style.fontFamily = 'Iosevka,' + e.style.fontFamily\
-          \\n    })\
-          \\n    document.querySelectorAll('code').forEach(e => { \
-          \\n      e.style.fontFamily = 'Iosevka,' + e.style.fontFamily\
-          \\n    })\
-          \\n  }\
-          \\n  if (fontFamilies.includes('CMUSerifRoman')) {\
-          \\n    document.body.style.fontFamily = 'CMUSerifRoman' + document.body.style.fontFamily\
-          \\n  }\
-          \\n});"
+  script_
+    "document.fonts.ready.then(fonts => {\
+    \\n  let fontFamilies = Array.from(fonts.values()).map(f => f.family)\
+    \\n  if (fontFamilies.includes('Iosevka')) {\
+    \\n    document.querySelectorAll('pre').forEach(e => { \
+    \\n      e.style.fontFamily = 'Iosevka,' + e.style.fontFamily\
+    \\n    })\
+    \\n    document.querySelectorAll('code').forEach(e => { \
+    \\n      e.style.fontFamily = 'Iosevka,' + e.style.fontFamily\
+    \\n    })\
+    \\n  }\
+    \\n  if (fontFamilies.includes('CMUSerifRoman')) {\
+    \\n    document.body.style.fontFamily = 'CMUSerifRoman' + document.body.style.fontFamily\
+    \\n  }\
+    \\n});"
 
 padLight :: Attribute
 padLight = class_ "pad-light"
