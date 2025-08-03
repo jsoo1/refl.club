72a2fd6277b59c7d37bc11da5249ba5c0183333c
John Soo <jsoo1@asu.edu>
2025-08-03 01:56:05 -0600

    .dir-locals.el: Remove unused command config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,14 +1,4 @@
-((nil
-  . ((eval
-      . (setq
-         projectile-project-compilation-cmd
-         "cabal new-build"
-         projectile-project-run-cmd
-         "cabal new-run exe:refl-club -- .static"
-         haskell-process-wrapper-function
-         (lambda (argv)
-           (append `("env" "-u" "GHC_PACKAGE_PATH") argv))))))
- (makefile-mode . ((tab-width . 4)))
+((nil . (compilation-command "nix build -L --no-link --print-out-paths"))
  (haskell-mode
   . ((haskell-process-type . cabal-repl)
      (haskell-mode-stylish-haskell-path . "ormolu")
